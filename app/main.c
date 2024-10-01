fa6f4050dc8505a1fb18573cd482662dbd93b741
Hill-Olivia <93399098+Hill-Olivia@users.noreply.github.com>
2024-10-01 15:24:27 +0000

    Added starter code

--- app/main.c
+++ app/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include "../src/lab.h"
 
 int main(void)
 {
-  printf("hello world\n");
+  char *line = (char *)NULL;
+  char *version = getVersion();
+  line = readline("What is your name?");
+  printf("Hello %s! This is the starter template version: %s\n", line, version);
   return 0;
 }
