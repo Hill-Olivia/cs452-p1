37cc784fb2d409b08cf5957cb26bd973db408563
Hill-Olivia <93399098+Hill-Olivia@users.noreply.github.com>
2024-10-03 02:10:42 +0000

    initial program arg set up

--- app/main.c
+++ app/main.c
@@ -4,14 +4,24 @@
 #include <readline/readline.h>
 #include "../src/lab.h"
 
-int main(void)
+
+// TODO: Get shell program to print version w/ -v flag 
+// parse args with getop
+// exit after
+
+int main(int argc, char *argv[]) // argc num of args + progname
 {
+
+  for (int i = 0; i < argc; i++) 
+  {
+      printf("Argument %d: %s\n", i, argv[i]);
+  }
   // char *line = (char *)NULL;
   // char *version = getVersion();
   // line = readline("What is your name?");
   // printf("Hello %s! This is the starter template version: %s\n", line, version);
   // free(line);
   // free(version);
-  
+
   return 0;
 }
